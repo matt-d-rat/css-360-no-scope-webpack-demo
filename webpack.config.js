b750f7ad66052b4a989e4006847340c579836f86
Matt Fairbrass <matt.fairbrass@gmail.com>
2015-12-01 17:12:20 +1100

    Added basic webpack configuration file
    Basic configuration file for webpack, configuring the css-loader with module support turned on, the web pack development server and hot module support.

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var port = 8000;
+var srcPath = path.join(__dirname, '/src');
+var publicPath = '/build/';
+
+module.exports = {
+    port: port,
+    debug: true,
+    cache: true,
+    devtool: 'sourcemap',
+    entry: [
+        'webpack-dev-server/client?http://127.0.0.1:' + port,
+        'webpack/hot/only-dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, '/dist/build'),
+        filename: 'app.js',
+        publicPath: publicPath
+    },
+    devServer: {
+        contentBase: './src/',
+        hot: true,
+        port: port,
+        publicPath: publicPath,
+        noInfo: false
+    },
+    resolve: {
+        modulesDirectories: ['node_modules'],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+};
